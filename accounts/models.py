4bd01ab0c706655b2a330947439a71aa025865d3
karupaccho <k.utsugi021@gmail.com>
2021-09-21 16:06:57 +0900

    コミット

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,9 @@
-from typing import AbstractSet
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
 
 class CustomUser(AbstractUser):
 
     class Meta:
         verbose_name_plural = 'CustomUser'
+
+# Create your models here.
