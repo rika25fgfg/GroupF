0807c4de2ccf7e0fb254fe3623dab5912e5af7b7
odd-star <ksw2070134@stu.o-hara.ac.jp>
2021-09-28 21:11:05 +0900

    セットアップ

--- kuroiwa/views.py
+++ kuroiwa/views.py
@@ -1,6 +1,24 @@
+import logging
+
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views import generic
+from .forms import InquiryForm
+
 # Create your views here.
 
 class IndexView(generic.TemplateView):
     template_name ="index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm 
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+
+logger = logging.getLogger(__name__)
+
